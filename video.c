d9b17dc6512b3e14a2a6764bcbddcca2aa6e7f24
aimenboulsane-hue <aimenboulsane@gmail.com>
2025-11-27 16:34:01 +0100

    Update video.c

--- video.c
+++ video.c
@@ -10,7 +10,20 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
-
-   // YOUR CODE HERE - END
-   return 0;
+    long long movie_frames = (long long)durationMovie * fps;
+    long long credits_frames = (long long)durationCredits * fps;
+    long long colorFrameSize = (long long)w*h*3;
+    long long bwFrameSize = (long long)w*h*1;
+    long long totalBytesLL =movie_frames*colorFrameSize +credits_frames*bwFrameSize;
+    float totalBytes = (float) totalBytesLL;
+    if (strcmp(unit, "bt") == 0) {
+        return totalBytes;
+    } else if (strcmp(unit, "ko") == 0) {
+        return totalBytes / 1024.0f;
+    } else if (strcmp(unit, "mo") == 0) {
+        return totalBytes / (1024.0f * 1024.0f);
+    } else if (strcmp(unit, "go") == 0) {
+        return totalBytes / (1024.0f * 1024.0f * 1024.0f);
+    }
+   return totalBytes;
 }
