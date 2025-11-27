1dd53ecaefc4bfc7eda80cd599f76d1bd19e19e3
aimenboulsane-hue <aimenboulsane@gmail.com>
2025-11-27 16:03:35 +0100

    Update coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,19 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
-
+float sizeBits=(float)(w*h*24);
+   if(strcmp(unit,"bt")== 0){
+      return sizeBits;
+   }
+   else if (strcmp(unit,"ko")== 0){
+      return sizeBits/1024.0;
+         }
+   else if (strcmp(unit,"mo")== 0){
+      return sizeBits/(1024.0 * 1024.0);
+         }
+     else if (strcmp(unit,"go")== 0){
+      return sizeBits/(1024.0 * 1024.0*1024.0);
+         }
    // YOUR CODE HERE - END
-   return 0;
+   return -1;
 }
